e62cbed29a18b6b87cdf533456dcf65cef571962
ashleydonohoe <ashleyrdonohoe@gmail.com>
2018-03-02 21:59:45 -0500

    threads display posts

--- src/components/ForumThreadList.js
+++ src/components/ForumThreadList.js
@@ -7,34 +7,28 @@ import getThreadPosts from '../selectors/getThreadPosts';
 
 export class ForumThreadList extends React.Component {
     render() {
-        //TODO: Get all of thread's posts
         const filteredForum = filterForums(this.props.posts, this.props.match.params.forumId)[0];
         const posts = getForumThreads(filteredForum.posts);
         // Take forum name and thread id to get the posts for thread
-        const postsForThread  = getThreadPosts(posts, this.props.match.params.postId);
-        console.log(postsForThread);
-        // console.log(postsForThread[3]);
-        // Thread id = this.props.match.params.postId
-        // const filteredForum = filterForums(this.props.posts, this.props.match.params.id)[0]
-        // const forumName = filteredForum.name;
-        // const posts = getForumThreads(filteredForum.posts);
-        // console.log(posts);
-        //
+        const post  = getThreadPosts(posts, this.props.match.params.postId)[0];
+        const replies = getForumThreads(post.replies);
+        console.log(replies);
+
         return (
             <div className="content-container">
                 <div className="list-header">
-                    <h1>Posts for Thread</h1>
+                    <h1>Posts for Thread Titled: {post.title}</h1>
                 </div>
 
-                {/*<div className="list-body">*/}
-                    {/*{ posts.length === 0 ? (*/}
-                        {/*<div>No Posts Found!</div>*/}
-                    {/*) : (*/}
-                        {/*posts.map((post) => {*/}
-                            {/*return <ForumPostItem key={post.id} {...post} />*/}
-                        {/*})*/}
-                    {/*)}*/}
-                {/*</div>*/}
+                <div className="list-body">
+                    { replies.length === 0 ? (
+                        <div>No replies yet!</div>
+                    ) : (
+                        replies.map((reply) => {
+                            return <ForumPostItem key={reply.id} {...reply} />
+                        })
+                    )}
+                </div>
             </div>
         )
     }
