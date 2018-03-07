4db51742f1f322b6f887cc4f31ad57d6a863056f
ashleydonohoe <ashleyrdonohoe@gmail.com>
2018-03-06 19:59:34 -0500

    working on threads

--- src/components/ForumThreadList.js
+++ src/components/ForumThreadList.js
@@ -6,41 +6,58 @@ import getForumThreads from '../selectors/getForumThreads';
 import getThreadPosts from '../selectors/getThreadPosts';
 
 export class ForumThreadList extends React.Component {
+
     render() {
-        // TODO: Write some kind of function that does a lot of this data conversion with fewer method calls
-        const postId = (this.props.match.params.postId);
-        const post = getThreadPosts(this.props.posts, postId);
-        console.log(post);
+        const post = this.props.post;
+
+        return (
+            <div className="content-container">
+                <div className="list-header">
+                    <h1>Posts for Thread: { post ? post.title : ""}</h1>
+                </div>
+
+                <div className="list-body">
+                    { post === undefined ? (
+                        <div>No Post Found!</div>
+                    ) : (
+                        // return first post
+                        <ForumPostItem key={post.id} {...post}/>
+                    )
+                    }
+                </div>
+
+                {/*<div className="list-body">*/}
+                {/*<ForumPostItem key={post.id} {...post}/>*/}
+                {/*{ replies.length === 0 ? (*/}
+                {/*<div>No replies yet!</div>*/}
+                {/*) : (*/}
+                {/*replies.map((reply) => {*/}
+                {/*return <ForumPostItem key={reply.id} {...reply} />*/}
+                {/*})*/}
+                {/*)}*/}
+                {/*</div>*/}
+                {/*) :*/}
+                {/*<p>No post found</p>*/}
+                {/*}*/}
+
+            </div>
+        )
+
+        // const postId = (this.props.match.params.postId);
+        // const post = (getThreadPosts(this.props.posts, postId))[0];
+        // const title = post.title || 'No Title';
         // const filteredForum = filterForums(this.props.posts, this.props.match.params.forumId)[0];
         // const posts = getForumThreads(filteredForum.posts);
         // // Take forum name and thread id to get the posts for thread
         // const post  = getThreadPosts(posts, this.props.match.params.postId)[0];
         // const replies = getForumThreads(post.replies);
 
-        return (
-            <div className="content-container">
-                {/*<div className="list-header">*/}
-                    {/*<h1>Posts for Thread Titled: {post.title}</h1>*/}
-                {/*</div>*/}
-
-                {/*<div className="list-body">*/}
-                    {/*<ForumPostItem key={post.id} {...post}/>*/}
-                    {/*{ replies.length === 0 ? (*/}
-                        {/*<div>No replies yet!</div>*/}
-                    {/*) : (*/}
-                        {/*replies.map((reply) => {*/}
-                            {/*return <ForumPostItem key={reply.id} {...reply} />*/}
-                        {/*})*/}
-                    {/*)}*/}
-                {/*</div>*/}
-            </div>
-        )
     }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
     return {
-        posts: state.posts
+        post: getThreadPosts(state.posts, ownProps.match.params.postId)[0]
     }
 };
 
