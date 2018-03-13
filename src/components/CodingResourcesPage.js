3ff6809c4d28fca1a3b7c9e1bc2711c8c2c6b63b
ashleydonohoe <ashleyrdonohoe@gmail.com>
2018-03-12 23:25:38 -0400

    finished resource search functionality

--- src/components/CodingResourcesPage.js
+++ src/components/CodingResourcesPage.js
@@ -3,13 +3,33 @@ import ResourcesList from './ResourcesList';
 import resources from '../resourceData';
 
 class CodingResourcesPage extends React.Component {
+    state = {
+        searchTerm: '',
+        resources: resources
+    };
+
+    onChange = (e) => {
+        const search = e.target.value.toLowerCase();
+        const filteredResources = resources.filter((resource) => {
+            return resource.title.toLowerCase().includes(search) || resource.description.toLowerCase().includes(search);
+        });
+
+        this.setState({
+            searchTerm: search,
+            resources: filteredResources
+        });
+    };
+
     render() {
         return (
             <div className="content-container">
                 <div className="list-header">
                     <h1>Coding Resources</h1>
                 </div>
-                <ResourcesList resources={resources} />
+                <div className="list-header">
+                    <input className="text-input search-field" value={this.state.searchTerm} placeholder="Search for a resource here" onChange={this.onChange}/>
+                </div>
+                <ResourcesList resources={this.state.resources} />
             </div>
         )
     }
--- src/resourceData.js
+++ src/resourceData.js
@@ -3,7 +3,18 @@ const resources = [
         title: 'CSS-Tricks',
         url: 'https://css-tricks.com/',
         description: 'A great place to learn about modern CSS practices'
-    }
+    },
+    {
+        title: 'Mozilla Developer Network',
+        url: 'https://developer.mozilla.org/en-US/',
+        description: 'Trusted source for HTML, CSS, and JavaScript documentation'
+    },
+    {
+        title: 'Bootstrap',
+        url: 'http://getbootstrap.com/',
+        description: 'Popular CSS framework to make styling easier'
+    },
+
 ];
 
 export default resources;
