9cf3dd4aa538b81b2b13cd909256b353fcfcf12f
ashleydonohoe <ashleyrdonohoe@gmail.com>
2018-03-16 00:13:49 -0400

    made names clickable

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -22,13 +22,14 @@ class MessageList extends React.Component {
             <div className="message-list" id="message-list">
                 <ul>
                     {messages.map((message, index) => {
+                        const profileURL = `users/${message.user.uid}`;
                         if(message.user.uid !== currentUser.uid) {
                             return (
-                                <Linkify key={index} properties={{target: '_blank'}}><li><span className="username">{message.user.name}</span>: {message.text}</li></Linkify>
+                                <Linkify key={index} properties={{target: '_blank'}}><li><a target="_blank" href={profileURL}><span className="username">{message.user.name}</span></a>: {message.text}</li></Linkify>
                             )
                         } else {
                             return (
-                                <Linkify key={index} properties={{target: '_blank'}}><li><span className="username active-user">{message.user.name}</span>: {message.text}</li></Linkify>
+                                <Linkify key={index} properties={{target: '_blank'}}><li><a target="_blank" href={profileURL}><span className="username active-user">{message.user.name}</span></a>: {message.text}</li></Linkify>
                             )
                         }
                     })}
