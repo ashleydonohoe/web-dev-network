82c212bc097f23265757fda4dfee38ed721d547d
ashleydonohoe <ashleyrdonohoe@gmail.com>
2018-03-22 00:43:43 -0400

    working on edit functionality

--- src/components/ForumPostItem.js
+++ src/components/ForumPostItem.js
@@ -3,11 +3,17 @@ import moment from 'moment';
 import { Link } from 'react-router-dom';
 
 export const ForumPostItem = ({user, content, title, id, date, currentPath, isThread, isPoster, onDelete, isReply, likes, onLike}) => {
+    console.log(currentPath);
    if(isThread) {
        const replyId = isReply ? id : undefined;
+       console.log(currentPath);
+       // const postPath = `${match.params.forumId}/${id}`;
+       // console.log(currentPath);
+       // const url = isReply ? `` : postPath;
        return (
            <div className="list-item">
                <div>
+                   { isPoster ? <Link to={`${currentPath}/${id}`} className="button button-edit">Edit</Link> : ""}
                    { isPoster ? <button className="button button-delete" onClick={() => onDelete(replyId, isReply)}>Delete</button> : ""}
                    <h3 className="list-item__title">{title}</h3>
                    <p className="list-item__sub-title">Posted at {moment(date).format('MMMM Do, YYYY')} by {user.name ? <Link to={`/users/${user.uid}`}> {user.name}</Link> : "Unknown"}</p>
